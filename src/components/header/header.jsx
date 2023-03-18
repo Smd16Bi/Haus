ce9a9b9e7d98bfab685c911ce7ea870b825ee855
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-03-18 20:26:47 +0200

    finish header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/logo";
 import Announcer from "./announcerBar/announcer-bar";
 import { NavLink } from "react-router-dom";
@@ -7,6 +7,13 @@ import style from "./header.module.css";
 
 
 const Header = ({ state }) => {
+    const [open, setOpen] = useState(false)
+
+    const openMenu = (event) => {
+        let html = event.target.closest("html");
+        html.classList.toggle("overflow_hidden")
+        setOpen(!open)
+    }
     const leftMenu = state.navLeft.map(el => {
         return (
             <li className={style["header_item"]} key={el.id}>
@@ -32,11 +39,25 @@ const Header = ({ state }) => {
                 </div>
                 <div className={`${style["header-wrap-item"]} ${style["header-wrap-item-logo"]}`}>
                     <NavLink to="/">
-                    <Logo />
+                        <Logo />
                     </NavLink>
                 </div>
                 <div className={`${style["header-wrap-item"]} ${style["header-wrap-item-list"]}`}>
-                <ul className={style["header_list"]}>
+                    <ul className={style["header_list"]}>
+                        {rightMenu}
+                    </ul>
+                </div>
+                <div onClick={openMenu}
+                    className={open ? `${style.burger} ${style.active}` : `${style.burger}`}
+                >
+                    <span className={style.line_1}></span>
+                    <span className={style.line_2}></span>
+                    <span className={style.line_3}></span>
+                </div>
+
+                <div className={open ? `${style.mobileMenu} ${style.active}` : style.mobileMenu }>
+                    <ul className={style.mobileMenu_list}>
+                        {leftMenu}
                         {rightMenu}
                     </ul>
                 </div>
