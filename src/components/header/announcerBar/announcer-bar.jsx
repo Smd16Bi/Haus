f5204ae32c8cc419df658c40696ad6a7b647d9b6
Anton Vasylchenko <vasylchenko.anton1997@gmail.com>
2023-03-19 12:37:01 +0200

    style mobile

--- src/components/header/announcerBar/announcer-bar.jsx
+++ src/components/header/announcerBar/announcer-bar.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+
 import style from "./announcer-bar.module.css";
+import 'swiper/css';
 
 
-const Announcer = ({text}) => {
+const Announcer = ({ text }) => {
+    console.log(text);
+    let slides = text.map((el, index) => {
+
+        return <SwiperSlide key={index}>
+            <p className={style['announcer-bar__info']}>{el.slide}</p>
+        </SwiperSlide>
+    })
     return (
         <div className={style["announcer-bar"]}>
-            <div className={ style["announcer-bar"] + " page_container"}>
-                <p className={style['announcer-bar__info']}>{text.text}</p>
+            <div className={style["announcer-bar"] + " page_container"}>
+                <Swiper
+                    loop={true}
+                    className="mySwiper"
+                    spaceBetween={30}
+                    slidesPerView="1"
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    modules={[Autoplay]}
+                >
+                    {slides}
+                </Swiper>
             </div>
         </div>
     )
--- src/store/store.js
+++ src/store/store.js
@@ -7,7 +7,11 @@ function id() {
 let store = {
     _state: {
         header: {
-            announcer: { text: "Free shipping on orders over $50" },
+            announcer: [
+                { slide: "Enjoy free delivery on orders above $50" },
+                { slide: "Get free shipping when you spend $50 or more" },
+                { slide: "Get free shipping on orders over $50 today!" },
+            ],
             navLeft: [
                 { id: id(), link: "Shop", path: "/shop"},
                 { id: id(), link: "Membership", path: "/membership"},
